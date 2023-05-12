db46b3dedae98a15952d27aabc035edb679e5a00
Andrew Gunn <agunn@ellsworth.com>
2023-05-12 14:05:51 -0500

    init

---
+++ src/Foundation/Features/Media/StandardFile.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Framework.DataAnnotations;
+
+namespace Foundation.Features.Media
+{
+    [ContentType(DisplayName = "Standard File", GUID = "646ECE50-3CE7-4F8B-BA33-9924C9ADC9C6", Description = "Used for standard file types such as Word, Excel, PowerPoint or text documents.")]
+    [MediaDescriptor(ExtensionString = "txt,doc,docx,xls,xlsx,ppt,pptx")]
+    public class StandardFile : MediaData
+    {
+        [Editable(false)]
+        public virtual string FileSize { get; set; }
+    }
+}
